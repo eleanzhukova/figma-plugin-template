3d5e9b65a1e6674f92afa4ba9a5cb337110662ab
eleanzhukova <eleanzhukova@gmail.com>
2024-08-07 12:42:19 +0300

    start project

---
+++ src/app/components/app.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export class App extends React.Component {
+
+  render() {
+    return (
+      <div className='plugin-content'>
+        <p>Create your code here...</p>
+      </div>
+    );
+  }
+};
